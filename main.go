c69bf6d510526cfd1de86d28033228853a37ce07
Rudraksh Karpe <rudraksh.karpe@gmail.com>
2023-09-10 23:30:55 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"https://github.com/rudrakshkarpe/genai-go"
+	"https://github.com/rudrakshkarpe/genai-go/streaming"
+)
+
+
+func main() {
+
+	generativemodel, err := generative.InitializeGenerativeModel() 
+}
